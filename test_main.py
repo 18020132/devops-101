7a7b4f6507624feb833d9187b87f7f5484287ca9
ducanhc <18020132@vnu.edu.vn>
2022-12-30 17:37:42 +0700

    update

--- test_main.py
+++ test_main.py
@@ -1,15 +1,17 @@
-from .main import app
+from main import app
 from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
 def test_result():
     response = client.post("/", json={"number_1": 10, "number_2": 2})
-    assert response.status_code == 200
+    print(response.status_code)
+    print(response.content)
     assert response.json() == {"result": 5.0}
 
 def test_zero():
     response = client.post("/", json={"number_1": 10, "number_2": 0})
+    print(response.status_code)
     assert response.json() == {"result": "error"}
 
 # def test_valid_1():
@@ -19,3 +21,5 @@ def test_zero():
 # def test_valid_2():
 #     response = client.post("/", json={"number_1": "a", "number_2": 2})
 #     assert response.json() == {"result": "error"}
+
+test_result()
